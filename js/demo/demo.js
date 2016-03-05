b2396528463631f31260f0c0350a5e6809bb09b2
LinJieLinLin <993353454@qq.com>
2016-03-05 17:07:21 +0800

    add slide-img

---
+++ js/demo/demo.js
@@ -0,0 +1,5 @@
+var directive = angular.module('demo', []);
+directive.controller('demoCtrl', ['$scope', function($scope) {
+    $scope.imgs = ['js/demo/1.png', 'js/demo/2.png','js/demo/3.png'];
+    $scope.imgs1 = ['js/demo/2.png', 'js/demo/1.png'];
+}]);
